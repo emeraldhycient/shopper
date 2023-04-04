e6fa6557d0446026d6b26b1c1e2ba76c4c753f55
emeraldhycient <igwezehycient86@gmail.com>
2023-04-04 22:56:29 +0100

    [update] switched to next link

--- src/components/home/ProductCard.tsx
+++ src/components/home/ProductCard.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Text} from '@chakra-ui/react'
 import { TproductCards } from '@/types'
+import Link from 'next/link'
 
 function ProductCard({ image, title, price, description }: TproductCards) {
 
     return (
-        <a href='/product/1/Poison ivy top (black and yellow)' className="">
+        <Link href='/product/1/Poison ivy top (black and yellow)' className="">
             <img src="https://i0.wp.com/yarnoverng.com/wp-content/uploads/2023/01/unnamed-21.jpg?zoom=2&resize=433%2C516&ssl=1" alt="" />
             <div className="py-5">
                 <Text fontSize='lg'>Poison ivy top (black and yellow)</Text>
                 <Text fontSize='sm' as="b">$200</Text>
             </div>
-        </a>
+        </Link>
     )
 }
 
