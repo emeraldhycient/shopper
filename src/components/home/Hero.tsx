9f3a2d3c9011badd6c021fdf0de9b4f329851ce3
emeraldhycient <igwezehycient86@gmail.com>
2023-04-05 16:29:56 +0100

    [update] added null display for cart qty

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -4,6 +4,7 @@ import { Tabs, TabList, Tab, TabPanels, TabPanel, Text } from '@chakra-ui/react'
 import Header from '../common/Header';
 import { AiOutlineArrowDown } from "react-icons/ai"
 import Typed from 'react-typed';
+import ScrollIntoView from 'react-scroll-into-view'
 
 
 function Hero() {
@@ -16,19 +17,20 @@ function Hero() {
                     <Text fontSize={["3xl", " 4xl"]} className='text-white text-left mb-7 leading-relaxed' >Shop with ease and take advantage of Pi to make your purchases.</Text>
                     <Text fontSize={['xs', 'md']} className='text-white text-left leading-relaxed' >
                         <Typed
-                        strings={['No more waiting for confirmations or worrying about fraud. Pi uses cutting-edge blockchain technology to ensure that your transactions are safe and secure.']}
+                            strings={['No more waiting for confirmations or worrying about fraud. Pi uses cutting-edge blockchain technology to ensure that your transactions are safe and secure.']}
                             typeSpeed={30}
                             loop
-                    /></Text>
-                    <a href="#products">
+                        /></Text>
+                    <ScrollIntoView selector="#products">
+
                         <button className='h-[50px] w-full md:w-[40%] bg-white text-black flex justify-center items-center mt-10 border font-bold'>
-                            Start Shopping <AiOutlineArrowDown size={22} className="ml-2 bounce font-bold"/>
+                            Start Shopping <AiOutlineArrowDown size={22} className="ml-2 bounce font-bold" />
                         </button>
-                    </a>
+                    </ScrollIntoView>
                 </div>
                 <div >
                     {/*srcSet="https://minepi.com/wp-content/uploads/2022/12/pi_video-6.webp 500w, https://minepi.com/wp-content/uploads/2022/12/pi_video-6-180x300.webp 180w, https://minepi.com/wp-content/uploads/2022/12/pi_video-6-7x12.webp 7w" */}
-                    <Image width="500" height="833" src="https://minepi.com/wp-content/uploads/2022/12/pi_video-6.webp" className="attachment-full size-full" alt="Pi Phone Image" decoding="async" loading="lazy"  sizes="(max-width: 500px) 100vw, 500px" />
+                    <Image width="500" height="833" src="https://minepi.com/wp-content/uploads/2022/12/pi_video-6.webp" className="attachment-full size-full" alt="Pi Phone Image" decoding="async" loading="lazy" sizes="(max-width: 500px) 100vw, 500px" />
                 </div>
             </div>
         </section>
