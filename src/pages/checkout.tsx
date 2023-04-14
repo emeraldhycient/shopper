aa50aa63f0965e0599e754e10d27037679d370a4
emeraldhycient <igwezehycient86@gmail.com>
2023-04-14 01:28:49 +0100

    [update] now forced pahe redirect

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import Header from '@/components/common/Header'
 import { Text, Flex, Input, Button } from '@chakra-ui/react'
+import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 
 function Checkout() {
+    const { push } = useRouter();
 
     const [name, setname] = useState("")
     const [email, setemail] = useState("")
@@ -12,13 +15,20 @@ function Checkout() {
     const [busstop, setbusstop] = useState("")
     const [country, setcountry] = useState("")
 
+    const handleSubmti = (e: any) => {
+        e.preventDefault();
+        setTimeout(() => {
+            push('/signin');
+        }, 1000)
+    }
+
     return (
         <section>
             <Header title={''} description={''} />
             <div className="h-screen w-full flex  py-12">
                 <div className="w-10/12 md:w-8/12 lg:6/12 h-[100vh] m-auto" >
                     <Text fontSize='3xl' as="b">Checkout</Text>
-                    <form className='mt-12'>
+                    <form className='mt-12' onSubmit={handleSubmti}>
                         <Text fontSize='xl' mb={4}>Contact Info</Text>
                         <Flex color='white' flexDirection={["column", "row"]} mb={4}>
                             <div className='w-full mr-4 mb-4'>
